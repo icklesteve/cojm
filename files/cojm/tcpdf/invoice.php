00ab620627af541620616480767ba60483629c50
Courier-Online <steve@cojm.co.uk>
2016-07-30 18:21:08 +0100

    fix #167

--- files/cojm/tcpdf/invoice.php
+++ files/cojm/tcpdf/invoice.php
@@ -1,4 +1,27 @@
 <?php
+
+/*
+    COJM Courier Online Operations Management
+	invoice.php - Create a PDF invoice
+    Copyright (C) 2016 S.Young cojm.co.uk
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// modded from :
+
 //============================================================+
 // File name   : example_001.php
 // Begin       : 2008-03-04
@@ -1071,12 +1094,39 @@ $html=$html. '<p>We have helped '.$clientrow['CompanyName'].' to save '.$ttablec
 ///////////////////////////////////////////////////////////
 
 
+$html.=$globalprefrow['invoicefooter3'].$newinvoiceref.$globalprefrow['invoicefooter4'];
+
+
+
+$html = str_replace ("&QUOT;", "&quot;", $html);
+$html = str_replace ("&AMP;", "&amp;", $html);
 
 
 
 
 
-$html=$html.$globalprefrow['invoicefooter3'].$newinvoiceref.$globalprefrow['invoicefooter4'];;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   if ($page=='createpdf') {
